e3a4c558e993a7a7ebb4a2d60ecea2c4c202185a
ZroC00l <giftgabanakgosi@gmail.com>
2022-09-22 11:55:08 +0200

    updates made

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
 /*Banner Component*/
 const Banner = ({
   purpose,
@@ -26,16 +28,17 @@ const Banner = ({
       <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3">
         {desc1} <br /> {desc2}
       </Text>
-      <Button fontSize="xl" bg="blue.300" color="white">
+      <Button fontSize="xl">
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
 );
 
-export default function Home() {
+export default function Home({ propertiesForRent, propertiesForSale }) {
+  console.log(propertiesForRent, propertiesForSale);
   return (
-    <div className="">
+    <Box>
       <h1>Hello World </h1>
       <Banner
         purpose="RENT A HOME"
@@ -47,6 +50,9 @@ export default function Home() {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
+      <Flex flexWrap="wrap">
+        {/*Get properties we want to map over(Rent a home) and then display them here */}
+      </Flex>
       <Banner
         purpose="BUY A HOME"
         title1="Find, Buy & Own Your"
@@ -57,6 +63,28 @@ export default function Home() {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-    </div>
+      <Flex>
+        {/*Get properties we want to map over(Buy a home) and then display them here */}
+      </Flex>
+    </Box>
   );
+}
+
+/* This is how nextjs fetches data from the api, this special function, works
+similar to how useEffect hook is used in a react environment*/
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
