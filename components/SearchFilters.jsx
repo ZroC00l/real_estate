5ac8807b828478de8a1145e05c2292357928be6a
ZroC00l <giftgabanakgosi@gmail.com>
2022-09-28 07:38:17 +0200

    update

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,27 +8,61 @@ import {
   Icon,
   Select,
   Button,
-} from "@chakra-ui/layout";
+} from "@chakra-ui/react";
+
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 
-const SearchFilters = () => {
-  const [filters] = useState();
+import { filterData, getFilterValues } from "../utils/filterData";
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+import noresult from "../assets/images/noresult.svg";
+
+export default function SearchFilters() {
+  const [filters] = useState(filterData);
   const [searchTerm, setSearchTerm] = useState("");
   const [locationData, setLocationData] = useState();
   const [showLocations, setShowLocations] = useState(false);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
-  const searchProperties = (filterValues) => {};
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value;
+      }
+    });
+
+    router.push({ pathname: path, query: query });
+  };
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const fetchData = async () => {
+        setLoading(true);
+        const data = await fetchApi(
+          `${baseUrl}/auto-complete?query=${searchTerm}`
+        );
+        setLoading(false);
+        setLocationData(data?.hits);
+      };
+
+      fetchData();
+    }
+  }, [searchTerm]);
 
   return (
     <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
       {filters?.map((filter) => (
         <Box key={filter.queryName}>
           <Select
-            onChange={{}}
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
             placeholder={filter.placeholder}
             w="fit-content"
             p="2"
@@ -77,7 +111,16 @@ const SearchFilters = () => {
             {showLocations && (
               <Box height="300px" overflow="auto">
                 {locationData?.map((location) => (
-                  <Box key={location.id} onClick={{}}>
+                  <Box
+                    key={location.id}
+                    onClick={() => {
+                      searchProperties({
+                        locationExternalIDs: location.externalID,
+                      });
+                      setShowLocations(false);
+                      setSearchTerm(location.name);
+                    }}
+                  >
                     {/*have to add onclick properties to box */}
                     <Text
                       cursor="pointer"
@@ -111,6 +154,4 @@ const SearchFilters = () => {
       </Flex>
     </Flex>
   );
-};
-
-export default SearchFilters;
+}
--- pages/index.js
+++ pages/index.js
@@ -36,45 +36,44 @@ const Banner = ({
   </Flex>
 );
 
-export default function Home({ propertiesForRent, propertiesForSale }) {
-  console.log(propertiesForRent, propertiesForSale);
-  return (
-    <Box>
-      <Banner
-        purpose="RENT A HOME"
-        title1="Rental Homes for "
-        title2="Everyone"
-        desc1="Explore Apartments, Homes, Villas"
-        desc2="and more"
-        buttonText="Explore Renting"
-        linkName="/search?purpose=for-rent"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-      />
-      <Flex flexWrap="wrap">
-        {/*Get properties we want to map over(Rent a home) and then display them here */}
-        {propertiesForRent.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </Flex>
-      <Banner
-        purpose="BUY A HOME"
-        title1="Find, Buy & Own Your"
-        title2="dream home"
-        desc1="Explore Apartments, Homes, Villas"
-        desc2="and more"
-        buttonText="Explore Buying"
-        linkName="/search?purpose=for-sale"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-      />
-      <Flex>
-        {/*Get properties we want to map over(Buy a home) and then display them here */}
-        {propertiesForSale.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </Flex>
-    </Box>
-  );
-}
+const Home = ({ propertiesForRent, propertiesForSale }) => (
+  //console.log(propertiesForRent, propertiesForSale);
+
+  <Box>
+    <Banner
+      purpose="RENT A HOME"
+      title1="Rental Homes for "
+      title2="Everyone"
+      desc1="Explore Apartments, Homes, Villas"
+      desc2="and more"
+      buttonText="Explore Renting"
+      linkName="/search?purpose=for-rent"
+      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+    />
+    <Flex flexWrap="wrap">
+      {/*Get properties we want to map over(Rent a home) and then display them here */}
+      {propertiesForRent.map((property) => (
+        <Property property={property} key={property.id} />
+      ))}
+    </Flex>
+    <Banner
+      purpose="BUY A HOME"
+      title1="Find, Buy & Own Your"
+      title2="dream home"
+      desc1="Explore Apartments, Homes, Villas"
+      desc2="and more"
+      buttonText="Explore Buying"
+      linkName="/search?purpose=for-sale"
+      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+    />
+    <Flex>
+      {/*Get properties we want to map over(Buy a home) and then display them here */}
+      {propertiesForSale.map((property) => (
+        <Property property={property} key={property.id} />
+      ))}
+    </Flex>
+  </Box>
+);
 
 /* This is how nextjs fetches data from the api, this special function, works
 similar to how useEffect hook is used in a react environment*/
@@ -94,3 +93,5 @@ export async function getStaticProps() {
     },
   };
 }
+
+export default Home;
